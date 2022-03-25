27fa5593a72e62821cdf17051936767215952ea5
Per Ekemark <per.ekemark@it.uu.se>
2022-03-25 13:38:36 +0000

    replace lock_data class with factory
    Using a class as the internal_field_type seemingly don't work (causes
    division by zero in tests). Replaced with a factory that produces an
    integer type to use as the internal field.

--- src/synchronization/global_tas_lock.hpp
+++ src/synchronization/global_tas_lock.hpp
@@ -20,20 +20,115 @@ namespace argo {
 		/** @brief a global test-and-set lock */
 		class global_tas_lock {
 			private:
-				class lock_data {
+				class lock_repr {
 					public:
-						static const argo::node_id_t init = -1;
-						argo::node_id_t last_user;
-						bool locked;
-						lock_data(bool locked = false, argo::node_id_t last_user = init)
-						: locked(locked), last_user(last_user) {};
-						bool operator==(const lock_data &other) {
-							return
-								this->last_user == other.last_user &&
-								this->locked == other.locked;
+						/** @brief lock representation type this class supports */
+						using lock_repr_type = std::uint64_t;
+					private:
+						/** @brief constant signifying lock is in an initial state and free */
+						static const lock_repr_type init = -2;
+						/** @brief constant signifying lock is taken */
+						static const lock_repr_type locked = -1;
+
+						/** @brief internal alias for determining node ID */
+						static argo::node_id_t inline self() { return backend::node_id(); }
+
+					public:
+						// Methods for information extraction. Side-effect free.
+
+						/** @brief Determines whether the lock field represents the an initial state.
+						 * @param lock_field The lock field to inspect.
+						 * @return True if the lock field is in an initial state, otherwise false.
+						 */
+						static bool inline is_init(lock_repr_type lock_field) { return lock_field == init; }
+
+						/** @brief Determines whether the lock field represents the a locked state.
+						 * @param lock_field The lock field to inspect.
+						 * @return True if the lock field is in a locked state, otherwise false.
+						 */
+						static bool inline is_locked(lock_repr_type lock_field) { return lock_field == locked; }
+
+						/** @brief Extracts the user information in the lock_field.
+						 * Undefined for a lock field in an initial state.
+						 * @param lock_field The lock field to inspect.
+						 * @return The node ID of the user encoded in the lock field.
+						 */
+						static argo::node_id_t inline get_user(lock_repr_type lock_field) { return lock_field; }
+
+						/** @brief Determines whether the lock field encodes the specified user.
+						 * Undefined for a lock field in an initial state.
+						 * @param lock_field The lock field to inspect.
+						 * @param node Node ID to check against.
+						 * @return True if the lock field encodes the specified user.
+						 */
+						static bool inline is_user(lock_repr_type lock_field, argo::node_id_t node) { return get_user(lock_field) == node; }
+
+						/** @brief Determines whether the lock field encodes the local node as the user.
+						 * Undefined for a lock field in an initial state.
+						 * @param lock_field The lock field to inspect.
+						 * @return True if the lock field encodes the local node as the user.
+						 */
+						static bool inline is_user_self(lock_repr_type lock_field) { return is_user(lock_field, self()); }
+
+
+						// Methods for creating lock fields with a specific state. Side-effect free
+
+						/** @brief Creates a lock field in a locked state.
+						 * @return A lock field in a locked state.
+						 */
+						static lock_repr_type inline make_locked() { return locked; }
+
+						/** @brief Creates a lock field in an unlocked state.
+						 * @param node Node ID to encode as the user of the lock.
+						 * @return A lock field in an unlocked state, encoded with the specified user.
+						 */
+						static lock_repr_type inline make_unlocked(argo::node_id_t node) { return node; }
+
+						/** @brief Creates a lock field in an unlocked state.
+						 * @return A lock field in an unlocked state, encoded with the local node as the user.
+						 */
+						static lock_repr_type inline make_unlocked() { return make_unlocked(self()); }
+
+						/** @brief Creates a lock field in an initial state.
+						 * @return A lock field in an initial state.
+						 */
+						static lock_repr_type inline make_init() { return init; }
+
+
+						// Methods for registering important lock events. May have side-effects.
+
+						/** @brief Registers intention to lock on the specified lock field with retries until successful.
+						 * @param lock_field Pointer to the lock field to lock on.
+						 */
+						static void inline lock_initiate(lock_repr_type *lock_field) {}
+
+						/** @brief Registers intention to lock on the specified lock field. This intent is allowed to fail.
+						 * @param lock_field Pointer to the lock field to lock on.
+						 * @param old_field The expected value of the lock field.
+						 * @return A locked lock field appropriate to become the new value of the targeted lock field.
+						 */
+						static lock_repr_type inline try_lock_initiate(lock_repr_type *lock_field, lock_repr_type old_field) {
+							return make_locked();
 						}
-						bool inline is_locked() { return locked; }
-						bool inline get_last_user() { return last_user; }
+
+						/** @brief Registers a successfull lock aquisition on the specified lock field.
+						 * This call should follow a call to @c try_lock_initiate with the same lock field.
+						 * @param lock_field Pointer to the lock field that has been locked.
+						 */
+						static void inline lock_success(lock_repr_type *lock_field) {}
+
+						/** @brief Registers a failed lock aquisition on the specified lock field.
+						 * This call should follow a call to @c try_lock_initiate with the same lock field.
+						 * @param lock_field Pointer to the lock field that could not be locked.
+						 */
+						static void inline lock_fail(lock_repr_type *lock_field) {}
+
+						/** @brief Registers intent to unlock the specified lock field.
+						 * @return An unlocked lock field appropriate to become the new value of the targeted lock field.
+						 */
+						static lock_repr_type inline unlock(lock_repr_type *lock_field) {
+							return make_unlocked();
+						}
 				};
 
 			public:
@@ -42,7 +137,7 @@ namespace argo {
 				 * @note this type may change without warning,
 				 *       user code must use this type alias
 				 */
-				using internal_field_type = lock_data;
+				using internal_field_type = std::uint64_t;
 
 				static_assert(sizeof(internal_field_type) <= 8,
 					"The internal lock field cannot exceed 8 bytes "
@@ -59,15 +154,20 @@ namespace argo {
 				 * @todo should be replaced with an ArgoDSM-specific atomic type
 				 *       to allow efficient synchronization over more backends
 				 */
-				global_lock_type lock_data;
+				global_lock_type lock_field;
 
 			public:
 				/**
 				 * @brief construct global tas lock from existing memory in global address space
 				 * @param f pointer to global field for storing lock state
 				 */
-				global_tas_lock(internal_field_type* f) : lock_data(global_lock_type(f)) {
-					*lock_data = internal_field_type(false);
+				global_tas_lock(internal_field_type* f) : lock_field(global_lock_type(f)) {
+					*lock_field = lock_repr::make_init();
+					/* TODO: Should this be an atomic store?
+					 * Every node has its own TAS lock with the same underlying data.
+					 * On creation, all nodes will write to the same location without synchronization.
+					 * This would constitute a data race.
+					 */
 				};
 
 				/**
@@ -76,13 +176,14 @@ namespace argo {
 				 *         false otherwise
 				 */
 				bool try_lock() {
-					auto old_data = backend::atomic::load(lock_data, atomic::memory_order::relaxed);
-					if (old_data.is_locked()) return false;
-					std::size_t self = backend::node_id();
-					auto new_data = internal_field_type(true, self);
-					bool success = backend::atomic::compare_exchange(lock_data, old_data, new_data, atomic::memory_order::relaxed);
+					auto old_field = backend::atomic::load(lock_field, atomic::memory_order::relaxed);
+					if (lock_repr::is_locked(old_field)) return false;
+					argo::node_id_t self = backend::node_id();
+					auto new_field = lock_repr::try_lock_initiate(lock_field.get(), old_field);
+					bool success = backend::atomic::compare_exchange(lock_field, old_field, new_field, atomic::memory_order::relaxed);
 					if (success) {
-						if (old_data.get_last_user() == self || old_data.get_last_user() == internal_field_type::init) {
+						lock_repr::lock_success(lock_field.get());
+						if (lock_repr::is_user_self(old_field) || lock_repr::is_init(old_field)) {
 							// The lock was not previously held by another node.
 							/* note: doing nothing here is only safe because we are using
 							 *       an SC for DRF memory model in ArgoDSM.
@@ -115,7 +216,7 @@ namespace argo {
 							 *       return to the initial unlocked state (init) wihtout
 							 *       also causing an argo acquire on all nodes.
 							 */
-						} else /* if (old_data.get_last_user() != internal_field_type::init && old_data.get_last_user() != self) */ {
+						} else {
 							// The lock was previously held by another node.
 							// TODO: Trigger node-wide release on previous owner (assuming there is one)
 							backend::acquire();
@@ -124,6 +225,7 @@ namespace argo {
 						return true;
 					}
 					else {
+						lock_repr::lock_fail(lock_field.get());
 						return false;
 					}
 				}
@@ -133,15 +235,15 @@ namespace argo {
 				 */
 				void unlock() {
 					backend::release();
-					std::size_t self = backend::node_id();
-					auto new_data = internal_field_type(false, self);
-					backend::atomic::store(lock_data, new_data);
+					auto new_field = lock_repr::unlock(lock_field.get());
+					backend::atomic::store(lock_field, new_field);
 				}
 
 				/**
 				 * @brief take the lock
 				 */
 				void lock() {
+					lock_repr::lock_initiate(lock_field.get());
 					while(!try_lock())
 						std::this_thread::yield();
 				}
