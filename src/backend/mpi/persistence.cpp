c935afd2cbeeadb26b2e0dad952fd86d6b09a826
Per Ekemark <per.ekemark@it.uu.se>
2022-03-30 14:34:58 +0000

    add durable_original struct

--- src/backend/mpi/persistence.cpp
+++ src/backend/mpi/persistence.cpp
@@ -12,14 +12,39 @@
 
 namespace argo::backend::persistence {
 
+	/** @brief Container of original data to use for recovery.
+	 * This data is generally only ever writen during normal operation
+	 * but may be used to find modifications. Usually another reference
+	 * is preferred, however.
+	 * @tparam entry_size Size (in bytes) of data entries.
+	*/
+	template<size_t entry_size>
+	struct durable_original {
+
+		/** @brief Container of original, unmodified data. */
+		char data[entry_size];
+
+		/** @brief Copy data into the original container.
+		 * @param source Pointer to the original data to copy from. Must be @c entry_size in length.
+		*/
+		void copy_data(char *source) {
+			memcpy(this->data, source, entry_size);
+		}
+
+	};
+
 	size_t undo_log::initialize(size_t offset) {
-		durable_mem = reinterpret_cast<unsigned char*>(argo::virtual_memory::allocate_mappable(entry_size, entry_size));
-		argo::virtual_memory::map_memory(durable_mem, entry_size, offset, PROT_READ|PROT_WRITE, argo::virtual_memory::memory_type::nvm);
-		return entry_size;
+		static_assert(sizeof(durable_original<entry_size>) == entry_size,
+			"The durable_original size doesn't match the entry_size.");
+		durable_mem = reinterpret_cast<durable_original<entry_size>*>(
+			argo::virtual_memory::allocate_mappable(entry_size, entries*sizeof(durable_original<entry_size>)));
+		argo::virtual_memory::map_memory(durable_mem, entries*sizeof(durable_original<entry_size>),
+			offset, PROT_READ|PROT_WRITE, argo::virtual_memory::memory_type::nvm);
+		return entries*sizeof(durable_original<entry_size>);
 	}
 
 	void undo_log::record_changes(location_t location, char *modified_data, char *original_data) {
-		memcpy(durable_mem, original_data, entry_size);
+		durable_mem[(reinterpret_cast<uintptr_t>(location)/entry_size)%entries].copy_data(original_data);
 	}
 
 }
--- src/backend/mpi/persistence.hpp
+++ src/backend/mpi/persistence.hpp
@@ -5,11 +5,19 @@
 
 namespace argo::backend::persistence {
 
+	template<size_t entry_size>
+	struct durable_original;	
+
 	class undo_log {
+
 		using location_t = argo::memory_t;
+
 		static const size_t entry_size = 4096; // TODO: From swdsm.cpp, should be imported from a header
 		static const size_t dirty_unit = 1; // TODO: Should be imported from a header
-		unsigned char *durable_mem;
+
+		static const size_t entries = 256; // TODO: Should be imported from elsewhere or be part of initialisation
+
+		durable_original<entry_size> *durable_mem;
 	public:
 		size_t initialize(size_t offset);
 
